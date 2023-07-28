114e326e86a1f46f1aa8c94ca192d38120f8b78f
abhi_win <f20190041@goa.bits-pilani.ac.in>
2023-07-28 22:46:01 +0530

    minor progress...

--- np_conv/conv2d.py
+++ np_conv/conv2d.py
@@ -3,13 +3,18 @@ import numpy as np
 
 
 
-def conv2d(img, weight, S=(1,1), D= (1,1)):
+def conv2d_nwhc(img, weight, S=(1,1), D= (1,1)):
   """
   implementation of convolutions with Numpy. Hopefully optimized
+
+
+  img : ndarray with nhwc shape 
+
   """
   N, HI, WI, CI = img.shape
   Ns, Hs, Ws, Cs = img.strides
   K = weight.shape[0], weight.shape[1]
+  CO = weight.shape[-1]
 
   HO = (HI -1 -D[0]*(K[0]-1))//S[0] +1
   WO = (WI -1 -D[1]*(K[1]-1))//S[1] +1
@@ -22,4 +27,25 @@ def conv2d(img, weight, S=(1,1), D= (1,1)):
 
            ).reshape(-1, K[0]*K[1]*CI)
   
-  print(temp)
+  out = temp @ weight.reshape(-1, CO)
+
+  return out.reshape(N, HO, WO, CO)
+
+
+
+def conv2d_nchw(img, weight, S, D):
+  N,CI, HI, WI = img.shape
+  Ns,Cs, Ws, Cs = img.strides
+  K = weight.shape[-2], weight.shape[-1]
+
+  CO  = weight.shape[1]
+
+  HO = (HI -1 -D[0]*(K[0]-1))//S[0] +1
+  WO = (WI -1 -D[1]*(K[1]-1))//S[1] +1
+
+  temp = 0
+  #TODO Add some meaningful code here.
+  
+  out= temp@ weight.reshape(...)
+
+  return out.reshape(N,CO, HO,WO)
--- np_conv/test.py
+++ np_conv/test.py
@@ -1,2 +1,28 @@
 # check all the implementations by comparing them to mainstream libraries. 
 #Also need to do time profiling and memory connsumption against normal convolutions
+
+
+
+#check against pytorch
+
+def pytorch_reference(img, weight,S =(1,1), D=(1,1)):
+    """
+    img: ndarray with nchw shape
+    weight:iokk shape
+    """
+    try:
+        import torch
+    except:
+        raise("To run this pytorch is required. Please visit pytorch.org to for installation related information.")
+    
+    return torch.nn.conv2d(img, weight,stride=S, dilation=D)
+
+
+def keras_reference(img, weight, S=(1,1), D= (1,1)):
+    try:
+        import tensorflow as tf
+    except:
+        raise("To run this, tensorflow is required. please visit tensorflow.org for installation related information.")
+    
+    return tf.keras.backend.conv2d(img, weight,strides=S, dilation_rate = D)
+     
