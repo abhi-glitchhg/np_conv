324348b5fe49b31efef281b11e7b8d851b82e6bd
abhi_win <f20190041@goa.bits-pilani.ac.in>
2023-08-28 22:33:19 +0530

    need to find this bug :)

--- np_conv/conv2d.py
+++ np_conv/conv2d.py
@@ -11,6 +11,23 @@ def conv2d_nhwc(img, weight, S=(1,1), D= (1,1)):
   img : ndarray with nhwc shape 
 
   """
+  # TODO 
+  # add checks for contiguous ndarray 
+  # it might be an issue if the array is not contiguous as we are directly picking values from the memory. (this assumes array is contiguous)
+  # again question arise that whether shoud we raise the issue or make the array contiguous ?
+  # -> on first thought we should raise the issue to the user. Let him handle the rough edges 
+  # -> in the error message add guide on how to make the array contigous. 
+  # also the array might be contiguous in fortran style; ahhhhhhhhhh
+  #  
+  assert img.flags['C_CONTIGUOUS'], "array should be c contiguous array, "
+  
+
+  try: 
+    assert len(img.shape) == 4
+  except:
+    assert len(img.shape) == 3, "expected array is of improper dimensions, fix it :) "
+    img = np.expand_dims(img, axis=0)
+
   N, HI, WI, CI = img.shape
   Ns, Hs, Ws, Cs = img.strides
   K = weight.shape[0], weight.shape[1]
@@ -18,7 +35,6 @@ def conv2d_nhwc(img, weight, S=(1,1), D= (1,1)):
 
   HO = (HI -1 -D[0]*(K[0]-1))//S[0] +1
   WO = (WI -1 -D[1]*(K[1]-1))//S[1] +1
-
   temp = as_strided(img ,
            
           shape = (N, HO, WO, K[0], K[1], CI),
@@ -26,7 +42,8 @@ def conv2d_nhwc(img, weight, S=(1,1), D= (1,1)):
            strides = (Ns, Hs*S[1], Ws+(S[0]-1)*img.itemsize*CI, Hs*D[1], Ws+CI*(D[0]-1)*img.itemsize, Cs ) 
 
            ).reshape(-1, K[0]*K[1]*CI)
-  
+
+  print(temp.shape)
   out = temp @ weight.reshape(-1, CO)
 
   return out.reshape(N, HO, WO, CO)
--- np_conv/test.py
+++ np_conv/test.py
@@ -30,18 +30,42 @@ def basic_test():
     """
     basic test suit, once we have enough functions; we will make a test folder and handle all test relted matters there only 
     """
-
-    Z = np.random.randn(5,224,224,3)
-
-    W = np.random.randn(4,4,3,6)
-
-    S =(1,1)
-    D=(1,1)
+    print("hi")
     import torch
-    Z_p = torch.tensor(Z).permute(0,3,1,2)
-    W_p = torch.tensor(W).permute(3,2,0,1)
+    for i in range(20):
+        Z = np.random.randn(5,224,224,3)
 
-    print(np.linalg.norm(pytorch_reference(Z_p,W_p,S,D).permute(0,2,3,1).contiguous().numpy() - conv2d_nhwc(Z,W,S,D)))
+        W = np.random.randn(4,4,3,6)
+    
+    
+        Z_p = torch.tensor(Z).permute(0,3,1,2)
+        W_p = torch.tensor(W).permute(3,2,0,1)
+        S =(1,1)
+        D=(1,1)
+
+        assert np.linalg.norm(pytorch_reference(Z_p,W_p,S,D).permute(0,2,3,1).contiguous().numpy() == conv2d_nhwc(Z,W,S,D))
+        print('we done 1')
+
+        S =(1,1)
+        D=(2,2)
+        assert np.linalg.norm(pytorch_reference(Z_p,W_p,S,D).permute(0,2,3,1).contiguous().numpy() == conv2d_nhwc(Z,W,S,D))
+        print('we done 2')
+        S =(3,3)
+        D=(2,2)
+        assert np.linalg.norm(pytorch_reference(Z_p,W_p,S,D).permute(0,2,3,1).contiguous().numpy() == conv2d_nhwc(Z,W,S,D))
+        print('we done 3')
+        S =(3,2)
+        D=(2,2)
+        assert np.linalg.norm(pytorch_reference(Z_p,W_p,S,D).permute(0,2,3,1).contiguous().numpy() == conv2d_nhwc(Z,W,S,D))
+        print('we done 4')
+        S =(3,6)
+        D=(2,1)
+        print(pytorch_reference(Z_p,W_p,S,D).permute(0,2,3,1).contiguous().numpy().shape)
+        print(conv2d_nhwc(Z,W,S,D).shape)
+        assert np.linalg.norm(pytorch_reference(Z_p,W_p,S,D).permute(0,2,3,1).contiguous().numpy() == conv2d_nhwc(Z,W,S,D)), 'dont match :( '
+        print('we done 5')
+
+        print("No errors found :) ")
 
 
 if __name__ == "__main__":
