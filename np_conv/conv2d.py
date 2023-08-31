34503a7917307f716cda54b22de2fa13d5e6358f
abhi_win <f20190041@goa.bits-pilani.ac.in>
2023-08-31 08:21:04 +0530

    nchw conv implementation going on

--- np_conv/conv2d.py
+++ np_conv/conv2d.py
@@ -36,7 +36,6 @@ def conv2d_nhwc(img, weight, S=(1,1), D= (1,1)):
 
            ).reshape(-1, K[0]*K[1]*CI)
 
-  print(temp.shape)
   out = temp @ weight.reshape(-1, CO)
 
   return out.reshape(N, HO, WO, CO)
@@ -44,18 +43,32 @@ def conv2d_nhwc(img, weight, S=(1,1), D= (1,1)):
 
 
 def conv2d_nchw(img, weight, S, D):
-  N,CI, HI, WI = img.shape
-  Ns,Cs, Ws, Cs = img.strides
-  K = weight.shape[-2], weight.shape[-1]
+  """
+  img: nchw ndarray
 
-  CO  = weight.shape[1]
+  weight: oikk shaped ndarray 
+
+  """
+
+
+  N,CI, HI, WI = img.shape
+  Ns,Cs, Hs, Ws = img.strides
+  K = weight.shape[-2:]
+
+  CO  = weight.shape[0]
 
   HO = (HI -1 -D[0]*(K[0]-1))//S[0] +1
   WO = (WI -1 -D[1]*(K[1]-1))//S[1] +1
 
-  temp = 0
-  #TODO Add some meaningful code here.
-  
-  out= temp@ weight.reshape(...)
+  temp = as_strided(img,
+                    
+                    shape = (N, HO, WO, CI, K[0], K[1]),
+
+                    strides = (Ns, Hs*S[1], Ws+(S[0]-1)*img.itemsize*CI, Cs, Hs*D[0], Ws+(D[1]-1)*img.itemsize)
+                    ).reshape(-1, K[0]*K[1]*CI)
+    
+  #print(temp)
+  out= temp@ weight.reshape(CO,-1)
 
   return out.reshape(N,CO, HO,WO)
+
--- np_conv/tests/test_conv2d.py
+++ np_conv/tests/test_conv2d.py
@@ -2,7 +2,8 @@
 #Also need to do time profiling and memory connsumption against normal convolutions
 
 import numpy as np
-from ..conv2d import conv2d_nchw, conv2d_nhwc
+import torch
+#from ..conv2d import conv2d_nchw, conv2d_nhwc
 
 
 def pytorch_reference(img, weight,S =(1,1), D=(1,1)):
@@ -16,6 +17,44 @@ def pytorch_reference(img, weight,S =(1,1), D=(1,1)):
         raise("To run this pytorch is required. Please visit pytorch.org to for installation related information.")
     
     return torch.nn.functional.conv2d(img, weight,stride=S, dilation=D)
+from numpy.lib.stride_tricks import as_strided
+
+
+def conv2d_nchw(img, weight, S=(1,1), D=(1,1)):
+  """
+  img: nchw ndarray
+
+  weight: oikk shaped ndarray 
+
+  """
+
+
+  N,CI, HI, WI = img.shape
+  Ns,Cs, Hs, Ws = img.strides
+  K = weight.shape[-2:]
+
+  CO  = weight.shape[0]
+
+  HO = (HI -1 -D[0]*(K[0]-1))//S[0] +1
+  WO = (WI -1 -D[1]*(K[1]-1))//S[1] +1
+
+  temp = as_strided(img,
+                    
+                    shape = (N, HO, WO, CI, K[0], K[1]),
+
+                    strides = (Ns, Hs*S[1], Ws+(S[0]-1)*img.itemsize*CI, Cs, Hs*D[0], Ws+(D[1]-1)*img.itemsize)
+                    )
+  
+  
+  print(temp.shape)
+  temp =temp.reshape(-1, K[0]*K[1]*CI)
+  print(temp.shape)
+  print(weight.shape)
+  weight = weight.reshape(CO, -1) 
+  print(weight.shape)
+
+  out = weight @ temp.T
+  return out.reshape(N,CO, HO,WO)
 
 
 
@@ -53,5 +92,17 @@ def test_pytorch():
         print(conv2d_nhwc(Z,W,S,D).shape)
         assert np.linalg.norm(pytorch_reference(Z_p,W_p,S,D).permute(0,2,3,1).contiguous().numpy() == conv2d_nhwc(Z,W,S,D)), 'dont match :( '
 
-        if __name__ == "__main__":
-            test_pytorch()
+if __name__ == "__main__":
+    #test_pytorch()
+
+    Z = np.random.randn(1,3,6,6)
+    W = np.random.randn(1,3,3,3)
+    np_out = conv2d_nchw(Z,W)
+    
+    Z_p = torch.tensor(Z)
+    W_p = torch.tensor(W)
+
+    out = pytorch_reference(Z_p, W_p)
+    print(out, '\n',np_out,'\n', np_out.dtype)
+    print(np.linalg.norm(pytorch_reference(Z_p,W_p).contiguous().numpy() == conv2d_nchw(Z,W)), 'dont match :( '
+    )
