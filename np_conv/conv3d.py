4f3b49f1bbbe6fa20fa22c407fa7215604e2ece9
Abhijit Deo <72816663+abhi-glitchhg@users.noreply.github.com>
2023-09-16 13:11:32 +0530

    Conv3d Implementation.  (#8)
    added conv3d implementation

---
+++ np_conv/conv3d.py
@@ -0,0 +1,51 @@
+from numpy.lib.stride_tricks import as_strided
+import numpy as np
+
+
+def conv3d_nchw(img, weight, S=(1,1,1), D=(1,1,1)):
+  """
+  img: ncdhw ndarray
+  weight: oikkk shaped ndarray 
+  """
+
+  N,CI,DI, HI, WI = img.shape
+  Ns,Cs,DS, Hs, Ws = img.strides
+  K = weight.shape[-3:]
+
+  CO  = weight.shape[0]
+
+  DO = (DI -1 -D[0]*(K[0]-1))//S[0] +1
+  HO = (HI -1 -D[1]*(K[1]-1))//S[1] +1
+  WO = (WI -1 -D[2]*(K[2]-1))//S[2] +1
+  temp = as_strided(img,
+                    shape = (N,DO, HO, WO, CI, K[0], K[1], K[2]),
+                    strides = (Ns,DS*S[0], Hs*S[1], Ws + (S[2]-1)*img.itemsize, Cs,DS*D[0], Hs*D[1], Ws+(D[2]-1)*img.itemsize)
+                    ).reshape(-1, K[0]*K[1]*K[2]*CI)
+  
+  out = (temp @ weight.reshape(CO, -1).T).reshape(N,DO,HO,WO,CO) # (N*DO,HO*WO, C)
+  return out.transpose((0,4,1,2,3))
+
+def conv3d_nhwc(img, weight, S=(1,1,1), D = (1,1,1)):
+  """
+  img : ndarray with ndhwc shape 
+  weight: ndarray with KKKIO shape
+  S: stride
+  D: Dilation
+  """
+  assert img.flags['C_CONTIGUOUS'], "array should be c contiguous array, "
+  N, DI, HI, WI, CI = img.shape
+  Ns, Ds , Hs, Ws, Cs = img.strides
+  K = weight.shape[0:3]
+  CO = weight.shape[-1]
+  DO, HO, WO = (DI -1 -D[0]*(K[0]-1))//S[0] +1 ,(HI -1 -D[1]*(K[1]-1))//S[1] +1 ,(WI -1 -D[2]*(K[2]-1))//S[2] +1
+
+  temp = as_strided(img ,        
+          shape = (N, DO, HO, WO, K[0], K[1],K[2], CI),
+           strides = (Ns, Ds*S[0], Hs*S[1], Ws+(S[2]-1)*img.itemsize*CI,Ds*D[0], Hs*D[1], Ws+(D[2]-1)*img.itemsize*CI , Cs ) 
+
+           ).reshape(-1, K[0]*K[1]*K[2]*CI)
+  
+  out = temp @ weight.reshape(-1, CO) # N,DO,HO,WO,CO
+
+  return out.reshape(N,DO, HO, WO, CO)
+  
---
+++ np_conv/test_conv3d.py
@@ -0,0 +1,103 @@
+print("hello")
+import numpy as np
+import torch
+from conv3d import conv3d_nchw, conv3d_nhwc
+from functools import partial
+from numpy.testing import assert_allclose as assert_np
+
+assert_np = partial(assert_np, rtol=1e-05)
+
+def pytorch_reference_conv3d(img, weight,S =(1,1,1), D=(1,1,1)):
+    """
+    img: ndarray with nchw shape
+    weight:iokk shape
+    """
+    try:
+        import torch
+    except:
+        raise("To run this pytorch is required. Please visit pytorch.org to for installation related information.")
+    return torch.nn.functional.conv3d(img, weight,stride=S, dilation=D)
+
+
+
+def test_pytorch():
+    """
+    basic test suit, once we have enough functions; we will make a test folder and handle all test relted matters there only 
+    """
+    it = 0
+    import torch
+
+    for i in range(1):
+        for height in [32,64,127]:
+            for width in [32,64]:
+                for depth in [16, 20]:
+                    for kernel1 in [2,1]:
+                        for kernel2 in [3,7,5]:
+                            for kernel3 in [3,7,5,2]:
+                                for ic in [1,3,7]:
+                                    for oc in [4,7,3]:
+                                        print(f"{it} th iteration:  {depth} {height} {width} {kernel1} {kernel2} {ic} {oc}")
+
+                                        
+                                
+                                        Z1 = np.random.randn(5,depth,height,width,ic)
+                                        W1 = np.random.randn(kernel1,kernel2, kernel3,ic, oc)
+                                    
+                                        Z_p_1 = torch.tensor(Z1).permute(0,4,1,2,3) #ncdhw
+                                        W_p_1 = torch.tensor(W1).permute(4,3,0,1,2) #oi
+                                
+                                        Z2 = np.random.randn(5,ic,depth, height,width)
+                                        W2 = np.random.randn(oc,ic,kernel1,kernel2, kernel3)
+
+                                        Z_p_2 = torch.tensor(Z2)
+                                        W_p_2 = torch.tensor(W2)
+
+
+                                        S =(1,1,1)
+                                        D=(1,1,1)
+                                        assert_np( conv3d_nhwc(Z1,W1,S,D), pytorch_reference_conv3d(Z_p_1,W_p_1,S,D).permute(0,2,3,4,1).contiguous().numpy())
+
+                                        assert_np( conv3d_nchw(Z2,W2,S,D), pytorch_reference_conv3d(Z_p_2,W_p_2,S,D).contiguous().numpy() )
+                                        
+                                        
+                                        S =(1,1,2)
+                                        D=(2,2,1)
+                                        
+                                        #assert_np( conv3d_nhwc(Z1,W1,S,D), pytorch_reference_conv3d(Z_p_1,W_p_1,S,D).permute(0,2,3,4,1).contiguous().numpy())
+
+                                        assert_np( conv3d_nchw(Z2,W2,S,D), pytorch_reference_conv3d(Z_p_2,W_p_2,S,D).contiguous().numpy() )
+
+                                        S =(1,1,4)
+                                        D=(3,3,2)
+
+                                        #assert_np( conv3d_nhwc(Z1,W1,S,D), pytorch_reference_conv3d(Z_p_1,W_p_1,S,D).permute(0,2,3,4,1).contiguous().numpy())
+
+                                        assert_np( conv3d_nchw(Z2,W2,S,D), pytorch_reference_conv3d(Z_p_2,W_p_2,S,D).contiguous().numpy() )
+                                        
+
+                                        S =(1,1,2)
+                                        D=(5,5,1)
+
+                                        #assert_np( conv3d_nhwc(Z1,W1,S,D), pytorch_reference_conv3d(Z_p_1,W_p_1,S,D).permute(0,2,3,4,1).contiguous().numpy())
+                                        
+                                        assert_np( conv3d_nchw(Z2,W2,S,D), pytorch_reference_conv3d(Z_p_2,W_p_2,S,D).contiguous().numpy() )
+                                        
+
+                                        S =(3,2,3)
+                                        D=(2,1,3)
+                                        
+
+                                        #ssert_np( conv3d_nhwc(Z1,W1,S,D), pytorch_reference_conv3d(Z_p_1,W_p_1,S,D).permute(0,2,3,4,1).contiguous().numpy())
+                                        assert_np( conv3d_nchw(Z2,W2,S,D),pytorch_reference_conv3d(Z_p_2,W_p_2,S,D).contiguous().numpy() ) 
+
+                                        S =(3,6,2)
+                                        D=(2,1,5)
+
+                                        #assert_np( conv3d_nhwc(Z1,W1,S,D), pytorch_reference_conv3d(Z_p_1,W_p_1,S,D).permute(0,2,3,4,).contiguous().numpy())
+                                        
+                                        assert_np(conv3d_nchw(Z2,W2,S,D),pytorch_reference_conv3d(Z_p_2,W_p_2,S,D).contiguous().numpy() )
+                                        it+=1
+
+
+if __name__ == "__main__":
+  test_pytorch()
